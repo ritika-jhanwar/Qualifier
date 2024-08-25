1b398d0a497f2feadc50281816d5e1248b348ad1
Ritika Jhanwar <97374584+ritika-jhanwar@users.noreply.github.com>
2024-08-25 11:59:26 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const app = express();
 app.use(bodyParser.json());
+app.use(cors());
 
 function processData(data) {
     const numbers = data.filter(item => !isNaN(item));
